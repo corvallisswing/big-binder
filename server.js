3fe78b418eee0102d85db8da462cb982b90f3599
Phil Manijak <github@exclsr.com>
2015-10-26 02:03:18 -0700

    Fix: Use relative path for views dir

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.set('views', './views')
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static('public'));
